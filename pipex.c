5e9b4d69a049d70040e5e12007e80300de4bdd8b
Oussama Lahmami <o.lahmami.2000@gmail.com>
2023-01-28 02:47:17 +0100

    wait()

--- pipex.c
+++ pipex.c
@@ -6,9 +6,25 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:35:51 by olahmami          #+#    #+#             */
-/*   Updated: 2023/01/26 18:11:53 by olahmami         ###   ########.fr       */
+/*   Updated: 2023/01/28 02:23:49 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+int main(int argc, char const *argv[])
+{
+	int id = fork(); // = 0
+	int n, i;
+
+	if (id == 0)
+		n = 1;
+	else
+		n = 6;
+	for (i = n; i < n + 5; i++)
+		printf("%d ",i);
+	if (id != 0)
+		wait(NULL);
+	
+	return 0;
+}
--- pipex.h
+++ pipex.h
@@ -6,7 +6,16 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:35:54 by olahmami          #+#    #+#             */
-/*   Updated: 2023/01/19 22:35:55 by olahmami         ###   ########.fr       */
+/*   Updated: 2023/01/28 02:23:44 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+#define PIPEX_H
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+#endif
