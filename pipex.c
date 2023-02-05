bd8a075bd3790bf6e3e99fbe2f537dfacacf1b1c
Oussama Lahmami <o.lahmami.2000@gmail.com>
2023-02-05 09:29:49 +0100

    dup

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:35:51 by olahmami          #+#    #+#             */
-/*   Updated: 2023/02/05 00:35:56 by olahmami         ###   ########.fr       */
+/*   Updated: 2023/02/05 07:59:16 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ char	**read_env(char **envp)
 	int i;
 
 	i = 0;
-	path =NULL;
-	split_path =NULL;
+	path = NULL;
+	split_path = NULL;
 	while (envp[i])
 	{
 		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
@@ -41,28 +41,50 @@ char	**read_env(char **envp)
 	return (split_path);
 }
 
-void cmd1(char *av1, char *av2)
+void cmd1(char *infile, char *cmd1, char **envp)
 {
-	int fd;
+	
 	
 }
 
 int main(int argc, char const *argv[], char **envp)
 {
-	char **split_path = read_env(envp);
+	char **split_path;
 	int fd[2];
 	pid_t pid1;
+	pid_t pid2;
 
+	split_path = read_env(envp);
 	if (pipe(fd) == -1)
 		return 1;
-	
 	if (argc == 5)
 	{
 		pid1 = fork();
+		if (pid1 < 0)
+			exit(1);
 		if (pid1 == 0)
-			cmd1(argv[1], argv[2]);		
-		
+		{
+			close(fd[0]);
+			dup2(fd[1], 1);
+			close(fd[1]);
+			cmd1(fd[1], argv[2], envp);
+		}
+		pid2 = fork();
+		if (pid2 < 0)
+			exit(1);
+		if (pid2 == 0)
+		{
+			close(fd[1]);
+			dup2(fd[0], 0);
+			close(fd[0]);
+			
+		}
+		close(fd[1]);
+		close(fd[0]);
+
+		waitpid(pid1, NULL, 0);
+		waitpid(pid2, NULL, 0);
 	}
-	printf("%s\n", split_path[1]);
+	
   return 0;
 }
