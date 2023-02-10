fa05ccfdc957a2e49e82a76a8263f0c3e357d838
Oussama Lahmami <oussama@kali>
2023-02-10 04:50:13 +0100

    cmd1

--- pipex.c
+++ pipex.c
@@ -6,84 +6,100 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:35:51 by olahmami          #+#    #+#             */
-/*   Updated: 2023/02/05 07:59:16 by olahmami         ###   ########.fr       */
+/*   Updated: 2023/02/10 04:49:07 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	**read_env(char **envp)
+char	**read_env(t_pipex *p, char **envp)
 {
 	char *path;
-	char **split_path;
 	size_t len;
-	int i;
 
-	i = 0;
+	p->i = 0;
 	path = NULL;
-	split_path = NULL;
-	while (envp[i])
+	p->split_path = NULL;
+	while (envp[p->i])
 	{
-		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+		if (ft_strncmp(envp[p->i], "PATH=", 5) == 0)
 		{
-			len = ft_strlen(envp[i]);
-			path = ft_substr(envp[i], 5, len);
+			len = ft_strlen(envp[p->i]);
+			path = ft_substr(envp[p->i], 5, len);
 		}
-		i++;
+		p->i++;
 	}
-	split_path = ft_split(path, ':');
-	i = 0;
-	while (split_path[i])
+	p->split_path = ft_split(path, ':');
+	p->i = 0;
+	while (p->split_path[p->i])
 	{
-		split_path[i] = ft_strjoin(split_path[i], "/");
-		i++;
+		p->split_path[p->i] = ft_strjoin(p->split_path[p->i], "/");
+		p->i++;
 	}
-	return (split_path);
+	return (p->split_path);
 }
 
-void cmd1(char *infile, char *cmd1, char **envp)
+char *cmd1(t_pipex *p, char **argv,char **envp)
 {
-	
-	
+	close(p->fd[0]);
+	p->infile = open(argv[1], O_RDONLY);
+	dup2(p->infile, 0);
+	dup2(p->fd[1], 1);
+	p->split_cmd = ft_split(argv[2], ' ');
+	if (access(p->split_cmd[0], F_OK | X_OK) < 0)
+	{
+		p->split_path = read_env(&p, envp);
+		p->i = 0;
+		while (p->split_path[p->i])
+		{
+			p->executable = ft_strjoin(p->split_path, p->split_cmd[0]);
+			if (access(p->executable, F_OK | X_OK) == 0)
+				return(p->executable);
+			p->i++;
+		}
+	}
+	else
+		p->executable = p->split_cmd[0];
+	return(p->executable);
+}
+
+void execution(t_pipex *p, char **envp)
+{
+	if (execve(p->executable, p->split_cmd, envp) == -1)
+	{
+		ft_putstr_fd("Error", 2);
+		exit(127);
+	}
 }
 
 int main(int argc, char const *argv[], char **envp)
 {
-	char **split_path;
-	int fd[2];
-	pid_t pid1;
-	pid_t pid2;
+	t_pipex p;
 
-	split_path = read_env(envp);
-	if (pipe(fd) == -1)
+	if (pipe(p.fd) == -1)
 		return 1;
 	if (argc == 5)
 	{
-		pid1 = fork();
-		if (pid1 < 0)
+		p.pid1 = fork();
+		if (p.pid1 < 0)
 			exit(1);
-		if (pid1 == 0)
+		if (p.pid1 == 0)
 		{
-			close(fd[0]);
-			dup2(fd[1], 1);
-			close(fd[1]);
-			cmd1(fd[1], argv[2], envp);
+			cmd1(&p, argv, envp);
+			execution(&p, envp);
 		}
-		pid2 = fork();
-		if (pid2 < 0)
+		p.pid2 = fork();
+		if (p.pid2 < 0)
 			exit(1);
-		if (pid2 == 0)
+		if (p.pid2 == 0)
 		{
-			close(fd[1]);
-			dup2(fd[0], 0);
-			close(fd[0]);
+			close(p.fd[1]);
+			dup2(p.fd[0], 0);
+			close(p.fd[0]);
 			
 		}
-		close(fd[1]);
-		close(fd[0]);
-
-		waitpid(pid1, NULL, 0);
-		waitpid(pid2, NULL, 0);
+		waitpid(p.pid1, NULL, 0);
+		waitpid(p.pid2, NULL, 0);
 	}
 	
   return 0;
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:35:54 by olahmami          #+#    #+#             */
-/*   Updated: 2023/02/03 04:50:45 by olahmami         ###   ########.fr       */
+/*   Updated: 2023/02/10 04:43:20 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,23 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include "Libft/libft.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct s_pipex
+{
+	char **split_path;
+	int i;
+	int fd[2];
+	pid_t pid1;
+	int infile;
+	char **split_cmd;
+	char *executable;
+	pid_t pid2;
+	int outfile;
+} t_pipex;
+
 
 #endif
